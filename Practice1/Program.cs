f2c70e3710f2f8ab2b66236d217e1b20b098281b
Kemal <Durakovic.Kemal11@gmail.com>
2015-05-01 20:47:55 -0500

    Practice 1 and vowels practice

---
+++ Practice1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = new int[10];
+            Console.WriteLine("Enter the Scores");
+            for (int i = 0; i < 10; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int smallest = array[0];
+            for (int i = 0; i < 10; i++)
+            {
+                if (array[i] < smallest)
+                {
+                    smallest = array[i];
+
+                }
+            }
+            int largest = array[9];
+            for (int i = 0; i < 10; i++)
+            {
+
+                if (array[i] > largest)
+                {
+                    largest = array[i];
+
+                }
+            }
+            int average = 0;
+
+            average = ((smallest + largest) / 2);
+            Console.WriteLine("the Lowest score is {0}", smallest);
+            Console.WriteLine("the Highest score is {0}", largest);
+            Console.WriteLine("the Average is {0}", average);
+            Console.Read();
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ vowels/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vowels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int cons = 0;
+            int total = 0;
+            //set up array for vowels
+           char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
+            // takes in sentance from user
+            Console.WriteLine("Enter a Sentence");
+            string sentence = Console.ReadLine();
+            // initiate loop
+            for (int i = 0; i < sentence.Length; i++)
+            {
+
+                
+                if (vowels.Contains(sentence[i]))
+                {
+                    total++;
+                }
+                else
+                {
+                    cons++;
+                }
+            }
+            Console.WriteLine("Ther are {0} vowels.", total);
+            Console.WriteLine("Ther are {0} consonants.", cons);
+            Console.ReadLine();
+
+
+
+
+        }
+
+
+        
+    
+    
+    
+    
+    }
+}
